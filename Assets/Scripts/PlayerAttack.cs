11e4c8303568a4c07c2e80605741aed2a90854a7
Craig Lu <Craig.Z.Lu@gmail.com>
2016-03-23 00:31:29 -0400

    added AI attack, stats, items, and base player attack

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+	private float attackSpeed;
+	private float damage;
+	private float nextAttack;
+	private GameObject hitText;
+	private Color color;
+	public Camera mainCamera;
+	void Start () {
+		hitText = new GameObject ();
+		mainCamera = GetComponentInChildren<Camera> ();
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = mainCamera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				if (hit.transform.tag == "Enemy") {
+					print ("I'm an enemy");
+
+					float distance = Vector3.Distance(hit.transform.gameObject.transform.position, transform.position);
+					print (distance); 
+					print (this.GetComponent<Stats> ().AttackRange);
+					if (distance <= this.GetComponent<Stats> ().AttackRange) {
+						print ("you can attack me");
+					}
+				}
+			}
+		}
+	}
+}
